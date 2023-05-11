d752b417a2a0e70cbdcee2360eacadc25b6d6757
onlyoreoo <94042043+onlyoreoo@users.noreply.github.com>
2023-05-11 15:33:45 +0300

    almost donr

---
+++ src/components/Login.js
@@ -0,0 +1,57 @@
+import React from "react";
+import firebase from "../firebase.js";
+import { Link } from 'react-router-dom'
+
+class Login extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email:'',
+            password:'',
+            error: null,
+        };
+    }
+
+    handleChange = e => {
+        this.setState({[e.target.name]: e.target.value});
+    }
+
+    handleSubmit = e => {
+        e.preventDefault();
+        const {email, username, password, error} = this.state;
+        firebase.auth().createUserWithEmailAndPassword(email, password).then(() => {
+                this.props.history.push('/');
+            })
+            .catch(error => {
+                this.setState({error});
+            });
+    }
+
+    render() {
+        const {email, username, password, error} = this.state;
+        return (
+            <div className="auth-container">
+                <h1>Login</h1>
+                <p>Login to access your account</p>
+                {error && <p className="error-message">{error.message}</p>}
+                <form onSubmit={this.handleSubmit}>
+                    <label htmlFor="email">Email address</label>
+                    <input type="text" name="email" id="email" value={email} onChange={this.handleChange}></input>
+                    <label htmlFor="password">Password</label>
+                    <input 
+                        type="password"
+                        name="password"
+                        id="password" 
+                        value={password}
+                        onChange={this.handleChange}
+                        >
+                    </input>
+                    <button className="submit">Login</button>
+                    <p>Don't have an account? <Link className="login-btn" to="/login">Register Here</Link></p>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default Login;
--- src/index.js
+++ src/index.js
@@ -3,13 +3,55 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Login from './components/Login';
+import Register from './components/Register';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import firebase, {auth, provider} from './firebase';
+
+class AppRouter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {user: null}
+  }
+  componentDidMount() {
+    auth.onAuthStateChanged(user => {
+      if(user){
+        this.setState({user});
+      }
+    })
+  }
+  logOutUser = () => {
+    firebase.auth().signOut()
+    .then(window.location = "/");
+  }
+  render() {
+    return (
+      <Router>
+        <div className="app">
+          <nav className="main-nav">
+            {!this.state.user &&
+              <div>
+                <Link to="/Login">Login</Link>
+                <Link to="/register">Register</Link>
+              </div>
+            }
+            {this.state.user &&
+              <a href='#!' onClick={this.logOutUser}>Log out</a>
+            }
+          </nav>
+        </div>
+        <Switch>
+          <Route path="/" exact render={() => <App user={this.state.user}/>} />
+          <Route path="/login" exact component={Login} />
+          <Route path="/register" exact component={Register} />
+        </Switch>
+      </Router>
+    );
+  }
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<AppRouter />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
